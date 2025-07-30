45348358411e1530916c4b50407e11eae2a7d614
unknown <mirzaanas815@gmail.com>
2025-07-30 06:30:05 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    
+  <div>
+<h1 className="text-rose-500 font-bold text-3xl" >Home</h1>
+<Button>Hello</Button>
+  </div>
+  );
+}
