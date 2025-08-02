a248161367de3afda7e5d1e8ce2314871e191083
unknown <mirzaanas815@gmail.com>
2025-08-02 12:18:40 +0530

    dashbaord

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import AddNewButton from "@/features/dashboard/components/AddNewButton";
+import AddRepoButton from "@/features/dashboard/components/AddRepoButton";
+import EmptyState from "@/components/ui/emptyState";
+
+const Page = () => {
+  const playgrounds: any[] = [];
+
+  return (
+    <>
+      <div className="flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
+          <AddNewButton />
+          <AddRepoButton />
+        </div>
+
+        <div className="mt-10 flex flex-col justify-start items-center w-full">
+          {playgrounds && playgrounds.length !== 0 ? (
+            <EmptyState
+              title="No Playgrounds"
+              description="Create a new playground to get started"
+              image="/no-playground.svg"
+            />
+          ) : (
+            <p>Playgournds Table</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
